716be26fc682ef1fa75de8026ba066f1a8892686
deabal0701 <deabal0701@naver.com>
2026-01-04 18:50:21 +0900

    InsightLink로 모두 변경

--- frontend/src/components/layout/AppHeader.vue
+++ frontend/src/components/layout/AppHeader.vue
@@ -54,7 +54,7 @@ const apiHealthy = computed(() => store.state.app.apiHealthy)
 const isDarkMode = computed(() => store.state.app.darkMode)
 
 const pageTitle = computed(() => {
-  return route.meta.title || 'DocuRAG'
+  return route.meta.title || 'InsightLink'
 })
 
 // 다크모드 토글
